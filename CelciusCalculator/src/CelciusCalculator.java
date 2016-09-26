00e0ceac85ff449cff3c0fb77fc537f9f3a05a9a
19khan <17hanj19@millshighsch.org>
2016-09-25 23:14:14 -0700

    Celsius calculator

---
+++ CelciusCalculator/src/CelciusCalculator.java
@@ -0,0 +1,9 @@
+//Kevin Han APCS2 8/29/16
+public class CelciusCalculator {
+	public static void main (String[]args) {
+		int f = -32 ;
+		double c = (5.0 / 9.0) * (f - 32);
+		
+		System.out.println (c);
+	}
+}
