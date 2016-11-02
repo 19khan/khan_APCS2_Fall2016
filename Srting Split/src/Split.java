c0bb9053edbb7710086c8f866e63ff733581dc9e
19khan <17hanj19@millshighsch.org>
2016-11-01 18:39:23 -0700

    String Split Final
    .

--- Srting Split/src/Split.java
+++ Srting Split/src/Split.java
@@ -1,50 +1,77 @@
+//APCS 2 Kevin Han
 import java.util.Arrays;
 import java.util.*;
 public class Split {
-	//array.lenght has to be greater than 3.
 
-	public static void main(String[] args){ 
+	public static void main(String[] args){
 		System.out.println("Give ingredients to sandwich.");
-		//Set up scanner. Ask for ingredients.
+		//Set up/prompt for scanner.
 		Scanner userInput = new Scanner (System.in);
-		String sandwichIngredients = userInput.next();
+		String sandwichIngredients = userInput.nextLine();
+		
+		String ingredients = "";
+		
+		String [] tempArray = sandwichIngredients.split(" ");
+		for (int i = 0; i < tempArray.length; i++){
+			ingredients += tempArray[i];
+		}
+		
 		//Establish array and set its content to .split array.
-		String [] sandwichArray = sandwichIngredients.split("bread");
+		
+		System.out.println(ingredients);
+		
+		String [] sandwichArray = ingredients.split("bread");
 		
 		System.out.println(Arrays.toString(sandwichArray));
 		
-		boolean check = checkSandwich(sandwichIngredients, sandwichArray);
-		if (check = false){
-			giveIngredients(sandwichArray);
+		//Check if sandwich is actually sandwich.
+		boolean check = checkSandwich(ingredients, sandwichArray);
+		if (check == false){ //when it isn't a sandwich
+			System.out.println ("This is not a sandwich.");
+		}else{ //when it is a sandwich.
+			giveIngredients(ingredients, sandwichArray);
 		}
 	}
 	
 	public static boolean checkSandwich (String ingredients, String [] sandwichArray){
-		//when the sandwich isn't a sandwich.
+		//all instances when the given ingredients do not make a sandwich.
+		
+		//Checking how many times the word "bread" occurs in ingredients.
+		String findStr = "bread";
+		int lastIndex = 0;
+		int count = 0;
+		while (lastIndex != -1){
+			lastIndex = ingredients.indexOf(findStr, lastIndex);
+			
+			if (lastIndex!= -1){
+				count ++;
+				lastIndex += findStr.length();
+			}
+		}
+		
 		if (sandwichArray.length <= 1
-			|| ingredients.indexOf("bread") == -1){
+			|| ingredients.indexOf("bread") < 0) {
+			return false;
+		}else if (count <2){
 			return false;
 		}else{
-			return true;	
+			return true;
 		}
 	}
 	
-	public static void giveIngredients (String [] sandwichArray){
-		if (sandwichArray.length <= 1
-				|| ingredients.indexOf("bread") == -1){
-				System.out.println("This is not a sandwich.");
-			}else if (ingredients.endsWith("bread") == true
-					&& ingredients.indexOf("bread") >= 0){
-				System.out.println(sandwichArray[sandwichArray.length -1]);	
-			}else{
-				//for loop counting up to length of array - 2 since last index is not needed.
-				//int i starts from 0 because of 0 base indexing.
-				for (int i = 0; i < sandwichArray.length-2; i++){
-					//add 1 to i since sandwichArray[0] is unneeded
-					System.out.print(sandwichArray[i+1]);
+	public static void giveIngredients (String ingredients, String [] ingredientsArray){
+		//converting given ingredients to ingredients in the middle of breads.		
+		if (ingredientsArray.length <= 2){
+			System.out.println(ingredientsArray[ingredientsArray.length-1]);
+		}else{
+			for (int i = 0; i < ingredientsArray.length-2; i++){
+			System.out.print(ingredientsArray[i + 1] + " ");
+			}
+		}
 	}
 }
-
+	
+		
 		//String.split();
 		//It's a method that acts on a string, <StringName>.split(<String sp>);
 		//Where sp is the string where the string splits
@@ -69,7 +96,3 @@ public class Split {
 		 * use String.split to split up the sandwich at the spaces, " ", and return what's in the middle of the sandwich and ignores what's on the outside
 		 * Again, what if it's a fancy sandwich with multiple pieces of bread?
 		*/
-
-		
-
-
